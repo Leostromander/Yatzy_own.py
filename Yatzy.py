6d5f6edf041f686d3789639d88be454fe7ec4bb6
Leostromander <leo.stromander@student.tabyenskilda.se>
2023-11-10 12:05:18 +0100

    Kommentering inför prototyp 4

--- Yatzy.py
+++ Yatzy.py
@@ -5,12 +5,12 @@ time = 0 # Skapa en variabel för vems tur det är
 turn_timer = 0 # Visar vilken tur det är
 
 def dice(): # tärningen
-    for i in range(amount_dice): #Starta en loop som ska upprepa ett visst antal gånger, bestämt av amount_dice.
+    for _ in range(amount_dice): #Starta en loop som ska upprepa ett visst antal gånger, bestämt av amount_dice.
         dice_throw = random.randint(1,6) # Slå en tärning och lagra resultatet i dice_throw
         dice_list.append(dice_throw) # Lägg till resultatet i listan dice_list
 
 def dice_choice(): # Denna funktion tar användarens inmatning för att bestämma vilka tärningar som ska kastas om. Den tar bort de valda tärningarna från listan, kastar om dem och visar resultatet för användaren.
-    global amount_dice
+    global amount_dice #"global" är en status på en variabel som betyder att den har samma värde överallt i programmet.
     while True:
         try:
             rerolling_dice = input("Which dice/dices would you like to throw again?: ")
@@ -29,13 +29,13 @@ def dice_choice(): # Denna funktion tar användarens inmatning för att bestämm
     print("Your new dices are:", dice_list)
     return dice_list
 
-def player_selection():
+def player_selection(): #Funktionen är till för att välja spelarna.
     global number_of_players
     global players
     players = {}
     global player_id
     player_id = {}
-    time_for_player_selection = 0
+    time_for_player_selection = 0 #En specifik klocka för att kunna skapa en "tag" på spelaren utan att störa med den globala klockan: "time".
     while True:
         try:
             number_of_players = int(input("How many players are there? (Choose between 2 and 4): "))
@@ -45,11 +45,11 @@ def player_selection():
                     player_tag = ("Player" + str(time_for_player_selection))
                     while True:
                         name = input("What is your name?: ")
-                        if players.get(name) == 0:
+                        if players.get(name) == 0: #Om namnet redan har skrivits betyder det att den har blivit tilldelad poäng vilket alltid har värdet 0. 
                             print("That name has already been selected.")
                         else:
                             players.update({name: points})
-                            player_id.update({player_tag: name})
+                            player_id.update({player_tag: name}) #Det används två olika dicts för att kunna använda sig av en förbestämd "tag" på spelaren men samtidigt ha sitt eget namn kopplat till ens poäng.
                             break
                 break
             else:
@@ -57,7 +57,7 @@ def player_selection():
         except ValueError:
             print("That is not an appropriate character.")
 
-def turn():
+def turn(): #main funktionen som använder alla andra funktioner.
     dice_list.clear()
     global amount_dice
     amount_dice = 5 # Ange antalet tärningar
@@ -67,7 +67,7 @@ def turn():
     if time == 1:
         turn_timer += 1
     global current_player
-    current_player = player_id.get("Player" + str(time))
+    current_player = player_id.get("Player" + str(time)) #Använder spelarens "tag" för att hitta och definera namnet på den nuvarande spelaren.
     print("----------------------------------------Turn " + str(turn_timer) + "----" + current_player + "------------------------------------------------------")
     print(players)
     print(current_player + " you throw your dice and get these values:")
@@ -88,7 +88,7 @@ def turn():
             break
     point_protocol()
 
-def point_protocol():
+def point_protocol(): #Funktionen som använder Yatzys poäng protocol för att fördela poäng.
     print("----------------------------------------Points Protocol--------------------------------------------------")
     print("You have thrown your dice three times and the result is:", dice_list)
     print("You are able to chooce these options for your dices:")
@@ -142,11 +142,9 @@ def point_protocol():
 
 player_selection()
 
-for i in range(3*number_of_players):
+for i in range(3*number_of_players): #Ser till att det spelas tre runder.
     turn()
     if time == number_of_players:
         time = 0
 
 print(players)
-
-print("End of test")
